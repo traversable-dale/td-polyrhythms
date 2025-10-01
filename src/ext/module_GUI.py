246c6d62eef0fb0eaa4067c442b0725a3c289cf9
Traversable Dale <johnsyzonenko@gmail.com>
2025-10-01 14:36:17 -0400

    re-visit project after 2 years | adds GUI, splash page, slider control, color hue_adjust | sets speed limit high/low | adds extensions to src folder

---
+++ src/ext/module_GUI.py
@@ -0,0 +1,41 @@
+import os
+
+class module_GUI :
+
+	def __init__ (self, ownerComp):
+		self.ownerComp = ownerComp
+		
+	def Test(self):
+		print("testing testing 1 2 3")
+		
+	def Startup(self):
+		#On startup TouchDesigner should open to splash screen
+		#After Splash screen there should be a loading screen
+		#Program opens fullscreen after that
+		print(f"GUI | Startup(): {me.name}")
+		op.splash.op('splash_ctrl')['opacity','val'] = 0
+		op.splash.op('filter_opacity').par.width = 0
+		op.config.op('timer_splash_startup').par.start.pulse()
+		op.splash.par.display = 1
+
+		op.GUI.par.display = 0
+		op.vis.par.opacity = 0
+		
+
+		op.config.op('timer_splash_startup').par.start.pulse()
+		
+		pass
+	
+	def ExitSplash(self):
+		op.config.op('timer_splash_exit').par.start()
+		return
+	
+	def OpenProgram(self):
+		#open program upon exiting splash screen
+		print(f"GUI | OpenProgram(): {me.name}")
+		op.GUI.par.display = 1
+		op.vis.par.opacity = 1
+		op.config.op('timer_GUI_open').par.start.pulse()
+		op.GUI.op('GUI_ctrl')['opacity','val'] = 1
+		op.audio.PlayAudio()
+		pass
